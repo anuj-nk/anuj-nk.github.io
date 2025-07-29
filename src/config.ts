36f0a2e70f1f16d3f2496e2db82a9374a4f5e879
Anuj Kamasamudram <63768806+anuj-nk@users.noreply.github.com>
2025-07-29 02:42:41 -0400

    Created Blog for Website
    Used slugs to develop Astro basic model that creates Blogs with BlogPost and Slugs to display blogs. Blogs are stored as Markdown files in the SSG.

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+// src/content/config.ts
+import { z, defineCollection} from 'astro:content';
+
+export const collections = {
+  blog: defineCollection({
+    schema: z.object({
+      title:       z.string(),
+      description: z.string(),
+      date:        z.date(),
+      tags:        z.array(z.string()),
+      image:       z.string().optional(),
+    }),
+  }),
+};
